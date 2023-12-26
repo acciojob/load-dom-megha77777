ab1103dc5d6044b3bdaf1fa0bf186deb915b6c43
megha77777 <141833704+megha77777@users.noreply.github.com>
2023-12-26 23:32:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function DOMLoadingfxn(params) {
+	document.body.textContent = "DOM load success";
+}
+document.addEventListener("DOMContentLoaded", DOMLoadingfxn)
